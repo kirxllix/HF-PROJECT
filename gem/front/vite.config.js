4a32ac7b442506470f24b4bc28c99b87f94630e7
kirxllix <kirxllix@gmail.com>
2025-11-19 23:31:13 -0800

    Delete gem directory

--- gem/front/vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js (Debe estar así)
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
