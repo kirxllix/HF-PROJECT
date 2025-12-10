0544294b1f93bb591db086b8bdc7d297ec7b9d5c
kirxllix <kirxllix@gmail.com>
2025-12-10 11:58:39 -0800

    Delete gem directory

--- gem/back/models/config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const ConfigSchema = new mongoose.Schema({
-    key: { type: String, required: true, unique: true }, // Ej: 'home_title'
-    value: { type: String, required: true }              // Ej: 'Nuestros Favoritos'
-});
-
-module.exports = mongoose.model('Config', ConfigSchema);
